2cf5d38b483ec83d6ac4938e0b0bc6e8c977dcd8
Luis Lira <luislira1029@gmail.com>
2023-08-03 17:11:46 -0600

    Add properties

--- src/main/java/com/lliramx/ms/placeholder/repository/BookRepository.java
+++ src/main/java/com/lliramx/ms/placeholder/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.lliramx.ms.placeholder.repository;
 
 import com.lliramx.ms.placeholder.model.Book;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,13 +10,16 @@ import java.util.Optional;
 @ApplicationScoped
 public class BookRepository {
 
+    @ConfigProperty(name = "books.genre", defaultValue = "Sci-Fi")
+    private String genre;
+
     public List<Book> getAllBooks() {
         return List.of(
-            new Book(1, "Java 1.8", "Antonio 1", 2020, "IT"),
-            new Book(2, "Java 11", "Antonio 2", 2021, "IT"),
-            new Book(3, "Java 17", "Antonio 3", 2022, "IT"),
-            new Book(4, "Java 21", "Antonio 4", 2023, "IT"),
-            new Book(5, "Java 20", "Antonio 5", 2024, "IT")
+            new Book(1, "Java 1.8", "Antonio 1", 2020, genre),
+            new Book(2, "Java 11", "Antonio 2", 2021, genre),
+            new Book(3, "Java 17", "Antonio 3", 2022, genre),
+            new Book(4, "Java 21", "Antonio 4", 2023, genre),
+            new Book(5, "Java 20", "Antonio 5", 2024, genre)
         );
     }
 
